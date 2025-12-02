e5ae92ff8a6e4eadb751322c2401afbc716f8f02
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-12-02 20:08:30 +0100

    add function to read from file

--- 1Day/secret_entrance.py
+++ 1Day/secret_entrance.py
@@ -2,3 +2,12 @@ from typing import Literal
 Direction = Literal["L", "R"]
 Move = tuple[Direction, int]
 DIAL_RANGE:tuple[int, int] = (0, 99)
+def get_puzzel_input() -> list[str]:
+    puzzel_input: list[str] = []
+
+    with open("puzzel_input.txt", "r") as file:
+        for line in file:
+            line = line.rstrip("\n")
+            puzzel_input.append(line)
+
+    return puzzel_input
